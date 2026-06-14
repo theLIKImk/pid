8bee2529f4a0b3c61ea4b99526f85b147423262d
LIKImk <103368018+theLIKImk@users.noreply.github.com>
2026-06-14 11:45:01 +0800

    Add files via upload

--- loadcfg.cmd
+++ loadcfg.cmd
@@ -1,8 +1,11 @@
 @echo off
 if not exist "%1" echo %1 not exist! & exit /b
 
+set LOADCFG_VERSION=1.1.0
+
 set CFGINI_DIR=%~dp1
 set CFGINI_FILE=%~nx1
+set CFGINI_THIS_INDEX=0
 
 pushd "%cd%"
 cd /d "%CFGINI_DIR%"
@@ -12,11 +15,16 @@ FOR /F "delims=* eol=#" %%f in (%CFGINI_FILE%) do (
 )
 
 set config_parent_tag=
+set /a CFGINI_THIS_INDEX-=1
 popd
 exit /b
 
 :setload
 	set obj=%*
-	if "%obj:~0,1%"=="[" set config_parent_tag=%obj:~1,-1%_
+	if "%obj:~0,1%"=="[" (
+		set config_parent_tag=%obj:~1,-1%_
+		set CFGINI_THIS_INDEX_%CFGINI_THIS_INDEX%=%obj:~1,-1%
+		set /a CFGINI_THIS_INDEX+=1
+	)
 	if not "%obj:~0,1%"=="[" set %config_parent_tag%%obj%
 goto :eof
